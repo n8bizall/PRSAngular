ff2cf6278a51c2bd3aeacfb26c354ce467904edd
Nate Bucher <n8bizall@yahoo.com>
2018-03-22 09:05:05 -0400

    commit so i can pull from github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent } from './Support/home/home.component';
 import { AboutComponent } from './Support/about/about.component';
+import { UserListComponent } from './Users/user-list/user-list.component';
 
 const routes: Routes = [
   {path:'', redirectTo: '/home', pathMatch: 'full' },
+  {path:'users/list', component: UserListComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'home', component: HomeComponent},
