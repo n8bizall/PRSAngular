6602a6e7ad1444e7e9f6bc1aa40dc892131fd49e
Greg doud <gpdoud@gmail.com>
2018-03-21 21:52:45 -0400

    Fixed error of missing @angular/* and others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ const routes: Routes = [
   {path:'', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'home', component: HomeComponent},
-  {path:'**', component: '/home'}
+  {path: '**', component: HomeComponent }
 ];
 
 @NgModule({
